4b2d9d8eb45dfd70a5ef4b0e32d788e4287881d9
YagovkinSI <sorpionupgrade@mail.ru>
2023-06-10 10:01:31 +0400

    [FEATURE-2] Separate Configure method in Startup file

--- YAGO.WebProjectTemplate.Web/Startup.cs
+++ YAGO.WebProjectTemplate.Web/Startup.cs
@@ -31,16 +31,7 @@ namespace YAGO.WebProjectTemplate.Web
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+			ExceptionHandling(app, env);
 
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
@@ -64,6 +55,20 @@ namespace YAGO.WebProjectTemplate.Web
 					spa.UseReactDevelopmentServer(npmScript: "start");
 				}
 			});
+		}
+
+		private static void ExceptionHandling(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 		}
 	}
 }
