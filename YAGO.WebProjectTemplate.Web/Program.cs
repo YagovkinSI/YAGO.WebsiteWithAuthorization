97d505c93d6d7ea57789a2a16546c2f0097002fd
YagovkinSI <sorpionupgrade@mail.ru>
2023-06-25 15:15:32 +0400

    [FEATURE-2] Rename solution

--- YAGO.WebProjectTemplate.Web/Program.cs
+++ YAGO.WebProjectTemplate.Web/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace YAGO.WebProjectTemplate.Web
+namespace YAGO.WebsiteWithAuthorization.Web
 {
 	public class Program
 	{
