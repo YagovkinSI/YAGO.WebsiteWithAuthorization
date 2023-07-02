f502601afa1eb572acfab8d9dd032aa9750b78d8
YagovkinSI <sorpionupgrade@mail.ru>
2023-07-02 07:30:52 +0400

    [FEATURE-3] Add database, Identity and AuthorizationService

---
+++ YAGO.Service.Authorization/Models/AuthorizationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YAGO.Service.Authorization.Models
+{
+	public class AuthorizationUser
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public DateTimeOffset Registration { get; set; }
+		public DateTimeOffset LastActivity { get; set; }
+	}
+}
