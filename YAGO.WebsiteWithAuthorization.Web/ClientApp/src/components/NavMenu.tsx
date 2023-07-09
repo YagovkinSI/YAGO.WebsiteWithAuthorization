cd8ee1d2eb01acbffe572cd0d5843b8b92b1d877
YagovkinSI <sorpionupgrade@mail.ru>
2023-07-09 08:24:26 +0400

    [FEATURE-3] Replace StringToColor function to ColorHelper

--- YAGO.WebsiteWithAuthorization.Web/ClientApp/src/components/NavMenu.tsx
+++ YAGO.WebsiteWithAuthorization.Web/ClientApp/src/components/NavMenu.tsx
@@ -4,6 +4,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import PersonIcon from '@mui/icons-material/Person';
 import { useNavigate } from 'react-router-dom';
 import { useGetCurrentUserQuery } from '../store/Authorization';
+import getColorFromString from '../utils/ColorHelper';
 
 interface Link {
     name: string,
@@ -140,30 +141,10 @@ const NavMenu: React.FC = () => {
         </Box>
     }
 
-    function stringToColor(string: string) {
-        let hash = 0;
-        let i;
-
-        /* eslint-disable no-bitwise */
-        for (i = 0; i < string.length; i += 1) {
-            hash = string.charCodeAt(i) + ((hash << 5) - hash);
-        }
-
-        let color = '#';
-
-        for (i = 0; i < 3; i += 1) {
-            const value = (hash >> (i * 8)) & 0xff;
-            color += `00${value.toString(16)}`.slice(-2);
-        }
-        /* eslint-enable no-bitwise */
-
-        return color;
-    }
-
     function stringAvatar(name: string) {
         return {
             sx: {
-                bgcolor: stringToColor(name),
+                bgcolor: getColorFromString(name),
             },
             children: `${name[0]}`,
         };
